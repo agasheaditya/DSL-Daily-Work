f75326be2da260d48bd74a9d75f8def60ba94dc4
Aditya Agashe <aditya.151154@srttc.ac.in>
2019-10-22 22:38:22 +0530

    Timeseries count version 2
    Code Cleaning

--- timeseries.py
+++ timeseries.py
@@ -2,28 +2,48 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_excel("data.xlsx")
-#df = df.drop(['Sales'],1)
 #print(df)
-#print(df.stack())
-#cols = (list(df.columns))
-#print(cols)
-state = df.unstack('Sales')
-set1= list(set(state))
-#print(set1)
-#print(len(set1))
 
-pivot = pd.pivot_table(df,index=["State","City","Product"],values="Sales",aggfunc=np.sum)#columns="Product"#
-df1 = pd.DataFrame(pivot)#, columns =pivot.index
+table = pd.pivot_table(df,index=["State","City","Product"],values="Sales",aggfunc=np.sum)#,columns=["State","City","Product","Sales"]
+#print(table)
+
+df1 = pd.DataFrame(table)
+#print(df1)
 df1.to_excel("output.xlsx")
-
-df2 = pd.read_excel("output.xlsx")
+df2 =  pd.read_excel("output.xlsx")
 print(df2)
-cols = list(df2.columns)
+
 count_TS = 0
-for i in cols:
+for i in df2.columns:
     if i =="":
         continue
     count_TS += df2[i].count()
 
 
 print("Total Timeseries = ",count_TS + 1 -(df2['Sales'].count()))
+
+##
+##import pandas as pd
+##import numpy as np
+##
+##df = pd.read_excel("data.xlsx",sheet_name="Sheet2")
+###print(df)
+##
+##table = pd.pivot_table(df,index=["State","City","Product"],values="Sales",aggfunc=np.sum)#,columns=["State","City","Product","Sales"]
+###print(table)
+##
+##df1 = pd.DataFrame(table)
+###print(df1)
+##df1.to_excel("output1.xlsx")
+##df2 =  pd.read_excel("output1.xlsx")
+##print(df2)
+##
+##count_TS = 0
+##for i in df2.columns:
+##    if i =="":
+##        continue
+##    count_TS += df2[i].count()
+##
+##
+##print("Total Timeseries = ",count_TS + 1 -(df2['Sales'].count()))
+##
